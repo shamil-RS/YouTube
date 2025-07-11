3e826b2bae382cc87484f56ddce2b4333e3e635b
shamil-CtR <93075490+shamil-CtR@users.noreply.github.com>
2025-07-11 13:28:32 +0300

    Readme.md

--- app/src/main/java/com/example/youtube/domain/model/Model.kt
+++ app/src/main/java/com/example/youtube/domain/model/Model.kt
@@ -1,82 +1,84 @@
 package com.example.youtube.domain.model
 
+import android.os.Parcelable
 import com.example.youtube.data.room.favorite.FavoriteVideo
 import com.example.youtube.data.room.watchedvideo.WatchedVideo
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
-@Serializable
+@Parcelize
 data class YouTubeSearchResponse(
     val kind: String,
     val items: List<YouTubeVideoItem>
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class YouTubeVideoItem(
     val kind: String,
     val id: VideoId,
     val snippet: Snippet
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class YouTubeVideoDetailsResponse(
     val kind: String,
     val items: List<VideoDetailsItem>
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class VideoDetailsItem(
     val id: String,
     val contentDetails: ContentDetails,
     val statistics: VideoStatistics,
     val snippet: Snippet?,
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class ContentDetails(
     val duration: String
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class VideoStatistics(
     val viewCount: Int,
     val likeCount: Int? = null,
     val dislikeCount: Int? = null
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class VideoId(
     val kind: String,
     val videoId: String
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class Snippet(
     val title: String,
     val categoryId: String? = null,
     val thumbnails: Thumbnails,
     val channelTitle: String? = null,
     val publishedAt: String? = null
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class Thumbnails(
     val default: Thumbnail,
     val medium: Thumbnail? = null,
     val high: Thumbnail? = null,
     val standard: Thumbnail? = null,
     val maxres: Thumbnail? = null
-)
+) : Parcelable
 
-@Serializable
+@Parcelize
 data class Thumbnail(
     val url: String
-)
+) : Parcelable
 
 data class VideoUIState(
     val listVideo: List<Video> = emptyList()
 )
 
-@Serializable
+@Parcelize
 data class Video(
     val id: String = "",
     val title: String = "",
@@ -90,7 +92,7 @@ data class Video(
     val dislikes: Int = 0,
     val posterUrl: String = "",
     var isSelected: Boolean = false,
-)
+) : Parcelable
 
 fun WatchedVideo.toVideo(): Video {
     return Video(
